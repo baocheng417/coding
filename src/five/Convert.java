1c9b2f53063564294e1260bb91626a6c7a0d73f5
Administrator <Administrator@PC201606141601>
2017-02-28 09:02:36 +0800

    2.28

--- src/five/Convert.java
+++ src/five/Convert.java
@@ -6,7 +6,66 @@ public class Convert
 	{
 		if(str == null || str.equals(""))
 		{
-			return 0;
+			return 0;  //不能转
 		}
+		
+		char[] cs = str.toCharArray();
+		
+		if(!isValid(cs))
+		{
+			return 0;   //不能转
+		}
+		
+		boolean posi = cs[0] == '-' ? false : true;
+		int minq = Integer.MIN_VALUE / 10;
+		int minr = Integer.MIN_VALUE % 10;
+		
+		int res = 0;
+		int cur = 0;
+		
+		for(int i = posi ? 0 : 1; i < cs.length; i++)
+		{
+			cur = '0' - cs[i];
+			if((res < minq) || (res == minq && cur < minr))
+			{
+				return 0;  //不能转
+			}
+			res = res * 10 + cur;
+		}
+		
+		if(posi && res == Integer.MIN_VALUE)
+		{
+			return 0;   //不能转
+		}
+		
+		return posi ? -res : res;
+	}
+	
+	public boolean isValid(char[] cs)
+	{
+		if(cs[0] != '-' || (cs[0] < '0' || cs[0] > '9'))
+		{
+			return false;
+		}
+		
+		if(cs[0] == '-' && (cs.length == 1 || cs[1] == '0'))
+		{
+			return false;
+		}
+		
+		if(cs[0] == '0' && cs.length > 1)
+		{
+			return false;
+		}
+		
+		for(int i = 1; i < cs.length; i++)
+		{
+			if(cs[i] < '0' || cs[i] > '9')
+			{
+				return false;
+			}
+		}
+		
+		return true;
 	}
 }
---
+++ src/five/RePlaceString.java
@@ -0,0 +1,62 @@
+package five;
+
+public class RePlaceString
+{
+	public void replace(char[] chas)
+	{
+		if(chas == null || chas.length == 0)
+		{
+			return ;
+		}
+		
+		int num = 0;
+		int len = 0;
+		
+		for(len = 0; len < chas.length && chas[len] != 0; len++)
+		{
+			if(chas[len] == ' ')
+			{
+				num++;
+			}
+		}
+		
+		int j = len + num * 2 -1;
+		
+		for(int i = len - 1; i != 0; i--)
+		{
+			if(chas[i] != ' ')
+			{
+				chas[j--] = chas[i];
+			}
+			else
+			{
+				chas[j--] = '0';
+				chas[j--] = '2';
+				chas[j--] = '%';
+			}
+		}
+	}
+	
+	public void modify(char[] chas)
+	{
+		if(chas == null || chas.length == 0)
+		{
+			return;
+		}
+		
+		int j = chas.length - 1;
+		
+		for(int i = chas.length - 1; i >= 0; i--)
+		{
+			if(chas[i] != '*')
+			{
+				chas[j--] = chas[i];
+			}
+		}
+		
+		for(;j > -1;)
+		{
+			chas[j--] = '*';
+		}
+	}
+}
